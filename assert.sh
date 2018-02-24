f7ede934b40fabba95558b70d06a7deb594c9f7f
Sion F <fandrick.sion@gmail.com>
2018-02-24 07:22:16 -0700

    Added support for commands in expected

--- assert.sh
+++ assert.sh
@@ -114,6 +114,23 @@ assert() {
     _assert_fail "expected $expected${_indent}got $result" "$1" "$3"
 }
 
+assert_cmd() {
+    # assert <command> <expected stdout> [stdin]
+    (( tests_ran++ )) || :
+    [[ -z "$DISCOVERONLY" ]] || return
+    expected="$(eval 2>/dev/null $2)" || true
+	result="$(eval 2>/dev/null $1 <<< ${3:-})" || true
+    if [[ "$result" == "$expected" ]]; then
+        [[ -z "$DEBUG" ]] || echo -n .
+        return
+    fi
+    result="$(sed -e :a -e '$!N;s/\n/\\n/;ta' <<< "$result")"
+    expected="$(sed -e :a -e '$!N;s/\n/\\n/;ta' <<< "$expected")"
+    [[ -z "$result" ]] && result="nothing" || result="\"$result\""
+    [[ -z "$expected" ]] && expected="nothing" || expected="\"$expected\""
+    _assert_fail "expected $expected${_indent}got $result" "$1" "$3"
+}
+
 assert_raises() {
     # assert_raises <command> <expected code> [stdin]
     (( tests_ran++ )) || :
